149fc5c389e295fcac063205e96a41ac5aeca7cc
v1lla <avs.zamora@gmail.com>
2022-11-23 19:05:19 +0100

    add method not allowed and general exception control

--- src/main/java/com/api/chess/management/exception/ExceptionController.java
+++ src/main/java/com/api/chess/management/exception/ExceptionController.java
@@ -4,12 +4,13 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionController{
 
   @ExceptionHandler(ResourceNotFoundException.class)
   public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
@@ -42,5 +43,27 @@ public class ExceptionController {
         request.getDescription(false));
     
     return new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
+  }
+  
+  @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+  public ResponseEntity<ErrorMessage> methodNotAllowedException(HttpRequestMethodNotSupportedException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.METHOD_NOT_ALLOWED.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.METHOD_NOT_ALLOWED);
+  }
+  
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorMessage> generalException(Exception ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.SERVICE_UNAVAILABLE.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.SERVICE_UNAVAILABLE);
   }
 }
