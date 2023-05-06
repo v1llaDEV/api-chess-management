5f77a513e1e3d11a8a3364a6598750b470ec74a4
v1llaDEV <avs.zamora@gmail.com>
2023-05-07 00:25:15 +0200

    first commit to migrate the proyect to a microservice proyect

--- src/main/java/com/api/chess/management/ProyectoGestionAjedrezApplication.java
+++ src/main/java/com/api/chess/management/ProyectoGestionAjedrezApplication.java
@@ -3,7 +3,9 @@ package com.api.chess.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ProyectoGestionAjedrezApplication extends SpringBootServletInitializer{
 
